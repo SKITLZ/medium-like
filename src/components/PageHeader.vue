49e904712ec66f9a5ca431c0321426a00e62e9d1
SKITLZ <gskitlz@gmail.com>
2020-03-09 02:08:18 +0300

    Add PageHeader

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="page-header">
+  <b-navbar wrapper-class="container" type="is-primary">
+    <template slot="start">
+      <b-navbar-item tag="router-link" to="/">Главная</b-navbar-item>
+    </template>
+
+    <template slot="end">
+      <b-navbar-item v-if="isLoggedIn" @click="logOut">Выйти</b-navbar-item>
+      <b-navbar-item v-else tag="router-link" to="/login/">Войти</b-navbar-item>
+    </template>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'PageHeader',
+
+  computed: {
+    isLoggedIn() {
+      // Заглушка
+      return false;
+    },
+  },
+
+  methods: {
+    logOut() {
+      // Заглушка
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+
+</style>

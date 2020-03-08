c3dc6b2e992e6d8ebc4dcfde94a1dfcb0b466481
SKITLZ <gskitlz@gmail.com>
2020-03-09 02:57:28 +0300

    Add LoginPage page and AuthForm template

---
+++ src/components/AuthForm.vue
@@ -0,0 +1,44 @@
+<template>
+<form class="auth-form">
+  <b-field label="Email">
+    <b-input type="email" v-model="email" />
+  </b-field>
+
+  <b-field label="Password">
+    <b-input type="password" v-model="password" />
+  </b-field>
+
+  <b-button
+    class="submit"
+    type="is-primary"
+    size="is-medium"
+    outlined
+    @click="logIn"
+  >Войти</b-button>
+</form>
+</template>
+
+<script>
+export default {
+  name: 'AuthForm',
+
+  data: () => ({
+    email: '',
+    password: '',
+  }),
+
+  methods: {
+    logIn() {
+      // Заглушка
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.auth-form {
+  .submit {
+    margin-top: 20px;
+  }
+}
+</style>
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="login-page">
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Авторизация</h1>
+      <AuthForm />
+    </div>
+  </section>
+</div>
+</template>
+
+<script>
+import AuthForm from '@/components/AuthForm.vue';
+
+export default {
+  name: 'LoginPage',
+
+  components: {
+    AuthForm,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.login-page {
+  .title {
+    text-align: center;
+  }
+
+  form {
+    max-width: 600px;
+    margin: 0 auto;
+  }
+}
+</style>

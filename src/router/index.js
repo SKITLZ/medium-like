cb5816eea41f670b3ca47230263d8a835eccf52d
SKITLZ <gskitlz@gmail.com>
2020-03-09 16:46:48 +0300

    Add LoginPage middleware

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import LoginPage from '../views/LoginPage.vue';
 
+import store from '../store';
+
 Vue.use(VueRouter);
 
 const routes = [
@@ -15,6 +17,11 @@ const routes = [
     path: '/login/',
     name: 'LoginPage',
     component: LoginPage,
+    beforeEnter: (to, from, next) => {
+      // Запрещает авторизованным пользователям переходить на страницу авторизации
+      if (store.getters['user/isLoggedIn']) next('/');
+      else next();
+    },
   },
 ];
 

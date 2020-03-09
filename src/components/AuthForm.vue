1dc0469e91d24c9bb8417cf0d5e93c67989349f3
SKITLZ <gskitlz@gmail.com>
2020-03-09 03:26:00 +0300

    Add basic AuthForm validation

--- src/components/AuthForm.vue
+++ src/components/AuthForm.vue
@@ -1,26 +1,47 @@
 <template>
-<form class="auth-form">
-  <b-field label="Email">
-    <b-input type="email" v-model="email" />
-  </b-field>
+<form class="auth-form" @submit.prevent="logIn">
+  <ValidationObserver ref="observer">
+    <ValidationProvider rules="required|email" name="Email" v-slot="{ errors, valid }">
+      <b-field
+        label="Email"
+        :type="{ 'is-danger': errors[0], 'is-success': valid }"
+        :message="errors"
+      >
+        <b-input type="email" v-model="email"></b-input>
+      </b-field>
+    </ValidationProvider>
 
-  <b-field label="Password">
-    <b-input type="password" v-model="password" />
-  </b-field>
+    <ValidationProvider rules="required" name="Password" v-slot="{ errors, valid }">
+      <b-field
+        label="Password"
+        :type="{ 'is-danger': errors[0], 'is-success': valid }"
+        :message="errors"
+      >
+        <b-input type="password" v-model="password"></b-input>
+      </b-field>
+    </ValidationProvider>
 
-  <b-button
-    class="submit"
-    type="is-primary"
-    size="is-medium"
-    outlined
-    @click="logIn"
-  >Войти</b-button>
+    <b-button
+      class="submit"
+      native-type="submit"
+      type="is-primary"
+      size="is-medium"
+      outlined
+    >Войти</b-button>
+  </ValidationObserver>
 </form>
 </template>
 
 <script>
+import { ValidationObserver, ValidationProvider } from 'vee-validate';
+
 export default {
   name: 'AuthForm',
+
+  components: {
+    ValidationObserver,
+    ValidationProvider,
+  },
 
   data: () => ({
     email: '',
@@ -29,7 +50,14 @@ export default {
 
   methods: {
     logIn() {
-      // Заглушка
+      this.$refs.observer.validate().then((success) => {
+        if (!success) {
+          console.log('not validated');
+          return;
+        }
+
+        console.log('validated!');
+      });
     },
   },
 };
